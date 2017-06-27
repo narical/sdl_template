51a32ece2447a36af0215f5c9f2f8f8fec71eebd
Joy Narical <jnarical@gmail.com>
2017-06-27 17:54:53 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+enum ErrType { VID_INIT, WIN_CREATE };
+void ErrorMsg ( ErrType );
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main (int argc, char **args)
+{
+	SDL_Window* window = nullptr;
+	SDL_Surface* screenSurface = nullptr;
+
+	if (SDL_Init( SDL_INIT_VIDEO ) < 0) ErrorMsg( VID_INIT );
+	else
+	{
+		window = SDL_CreateWindow
+			("Hello World",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+			
+		if ( window == nullptr ) ErrorMsg( WIN_CREATE );
+		
+		else
+		{
+			screenSurface = SDL_GetWindowSurface( window );
+			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0xFF, 0x00 ) );
+			SDL_UpdateWindowSurface( window );
+			SDL_Delay( 3000 );
+		}
+	}
+
+	SDL_DestroyWindow( window );
+	SDL_Quit();
+	return 0;
+}
+
+
+
+void ErrorMsg( ErrType error ) {
+	switch ( error )
+	{
+		case VID_INIT:
+			printf( "SDL initialization error: %s\n", SDL_GetError() );
+			break;
+			
+		case WIN_CREATE:
+			printf( "Window creation error: %s\n", SDL_GetError() );
+			SDL_Quit();
+			break;
+	}
+	exit(-1);
+}
